bd4aaea66de9bf206ddf69cc82c027063463896a
Chance Rutledge <chancerutledge2@gmail.com>
2020-03-12 07:38:31 -0700

    inital commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code away!
+const server = require('./server');
+
+const PORT = 5000;
+server.listen(PORT, () => console.log(`\n** Server Running on http://localhost:${PORT}**\n`));
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
+const userRouter = require('./users/userRouter.js');
+
+
+
+//define middleware
 const server = express();
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +14,14 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  // console.log(req);
+  const method = req.method;
+  const url = req.url;
+  const timestamp = req.timestamp;
+
+  console.log(`${method}, ${url}, ${timestamp}`);
+  next();
+}
 
 module.exports = server;
