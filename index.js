e5687095d8deb4fdc8a9149ed5c09e97f506430e
Chance Rutledge <chancerutledge2@gmail.com>
2020-03-12 16:51:59 -0700

    :deployment test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// code away!
+require("dotenv").config();
 const server = require('./server');
 
-const PORT =4000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => console.log(`\n** Server Running on http://localhost:${PORT}**\n`));
--- server.js
+++ server.js
@@ -9,7 +9,9 @@ server.use('/api/users', userRouter);
 server.use('/api/posts', postRouter)
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  const environment = process.env;
+  const port = process.env.PORT || 4000;
+  res.status(200).json({api: "up", port, environment });
 });
 
 //custom middleware
