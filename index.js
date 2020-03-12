da4737129a76714e24615cda52e76d55f6ce6f00
Chance Rutledge <chancerutledge2@gmail.com>
2020-03-12 12:15:54 -0700

    mvp

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // code away!
 const server = require('./server');
 
-const PORT = 5000;
+const PORT =4000;
 server.listen(PORT, () => console.log(`\n** Server Running on http://localhost:${PORT}**\n`));
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
-
-const userRouter = require('./users/userRouter.js');
-
-
-
-//define middleware
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
 const server = express();
+
+server.use(express.json());
 server.use(logger);
 server.use('/api/users', userRouter);
+server.use('/api/posts', postRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -16,15 +15,9 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-  // console.log(req);
-  const method = req.method;
-  const url = req.url;
-  const timestamp = req.timestamp;
-
-  console.log(`${method}, ${url}, ${timestamp}`);
+  const { method, originalUrl } = req;
+  console.log(`${method} to ${originalUrl}`);
   next();
 }
-
-
 
 module.exports = server;
